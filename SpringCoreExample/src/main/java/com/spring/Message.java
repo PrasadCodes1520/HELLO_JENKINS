e94660a187d51210814fdb898215458470ef49f5
PrasadCodes1520 <Prasadg.work247@gmail.com>
2024-02-08 14:46:16 +0530

    first commit

---
+++ SpringCoreExample/src/main/java/com/spring/Message.java
@@ -0,0 +1,7 @@
+package com.spring;
+
+public class Message {
+	void printMessage() {
+		System.out.println("Hello World");
+	}
+}
---
+++ SpringCoreExample/src/main/java/springcore_example/HelloWorld.java
@@ -0,0 +1,13 @@
+package springcore_example;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Hello, " + message);
+    }
+}
